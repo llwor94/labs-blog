d1a6a6226e1cce33d43a87fae96c89da5268f222
Lauren <llwor94@gmail.com>
2019-02-11 17:47:58 -0800

    init

---
+++ config.js
@@ -0,0 +1,38 @@
+'use strict';
+
+module.exports = {
+	url: 'https://lumen.netlify.com',
+	title: 'Labs Blog',
+	subtitle: `Lauren documenting her time throughout labs`,
+	copyright: '© All rights reserved.',
+	disqusShortname: '',
+	postsPerPage: 4,
+	googleAnalyticsId: 'UA-73379983-2',
+	menu: [
+		{
+			label: 'Articles',
+			path: '/',
+		},
+		{
+			label: 'About me',
+			path: '/pages/about',
+		},
+		{
+			label: 'Contact me',
+			path: '/pages/contacts',
+		},
+	],
+	author: {
+		name: 'Lauren Worthington',
+		photo: '/photo.jpg',
+		bio: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+		contacts: {
+			email: '#',
+			telegram: '#',
+			twitter: '#',
+			github: '#',
+			rss: '#',
+			vkontakte: '#',
+		},
+	},
+};
